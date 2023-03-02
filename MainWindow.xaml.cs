d9afad5d5e77c13cd5e2f678b8f85e693a5b184e
Aaron Verhaverbeke <aaron.verhaverbeke@gmail.com>
2023-03-02 18:00:40 +0100

    Gebruik maken van styles voor code cleanup Export functie toevoegen om data door te kunnen sturen naar andere programma's en het wiel.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Runtime.ConstrainedExecution;
 using System.Text;
@@ -26,13 +27,31 @@ namespace circle_display
     {
         public static double angle1 = 0.1013416985; // Hoek tussen de segmenten in radialen. (Moet nog in formule verwerkt worden.)
 
-        static int wheelCircle = 350; // Diameter van de kleinste cirkel van het wiel.
+        static int wheelCircle = 400; // Diameter van de kleinste cirkel van het wiel.
         public static int ledDistance = 25; // This is a the distance between the leds in a row.
 
         static int numberOfLeds = 16; // Aantal leds per rij / lengte van de ledstrip.
         static int numberOfSegments = 32; // Aantal segmenten waarin het halve of hele wiel is ingedeeld.
 
         public static int circleCenter = (wheelCircle + (ledDistance * 2) * (numberOfLeds + 1)) / 2; // Y-locatie van het centrum van het wiel.
+
+
+        PointF centerCirkel = new PointF(circleCenter, circleCenter);
+        PointF endCirkel;
+        PointF coord1;
+        PointF coord2;
+
+        struct APA102C
+        {
+            public double brightness;
+            public double red;
+            public double green;
+            public double blue;
+        }
+
+        APA102C[,] DataByte = new APA102C[32, 16];
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -55,24 +74,9 @@ namespace circle_display
             rectangle.Height = circleCenter;
             rectangle.Width = circleCenter * 2;
             rectangle.Margin = new Thickness(0, circleCenter, 0, 0);
-            rectangle.Fill = new SolidColorBrush(Colors.White);
             cnvsCircles.Children.Add(rectangle);
 
-            for (int j = 0; j < 32; j++)
-            {
-                for (int i = 0; i < 16; i++)
-                {
-                    System.Windows.Controls.CheckBox newChb = new CheckBox();
-
-                    newChb.Tag = (15 - i).ToString() + "," + (j).ToString();
-                    newChb.Height = newChb.Width = 30;
-                    newChb.Checked += NewChb_Checked;
-                    newChb.Unchecked += NewChb_Unchecked;
-                    newChb.Margin = new Thickness(j * 30 + 50 , i * 30 + circleCenter + 100, 0, 0);
-                    cnvsCircles.Children.Add(newChb);
-                }
-
-            }
+            
 
             for (int i = 0; i < 16; i++)
             {
@@ -80,14 +84,15 @@ namespace circle_display
                 Label labelChbRow = new Label();
                 labelChbRow.Content = i.ToString();
                 labelChbRow.Height = labelChbRow.Width = 30;
-                labelChbRow.Margin = new Thickness(20, (15 - i) * 30 + circleCenter + 95, 0, 0);
+                labelChbRow.HorizontalContentAlignment = HorizontalAlignment.Right;
+                labelChbRow.Margin = new Thickness(15, (15 - i) * 30 + circleCenter + 95, 0, 0);
                 cnvsCircles.Children.Add(labelChbRow);
 
                 // Zet de labels bij de cirkels met het aantal rijen aangeduid
                 Label labelCrkRow = new Label();
-                labelCrkRow.Content = i.ToString();
+                labelCrkRow.Content = (15 - i).ToString();
                 labelCrkRow.Height = labelCrkRow.Width = 30;
-                labelCrkRow.Margin = new Thickness(i * ledDistance + 20, circleCenter + 10, 0, 0);
+                labelCrkRow.Margin = new Thickness(i * ledDistance + 15, circleCenter + 10, 0, 0);
                 cnvsCircles.Children.Add(labelCrkRow);
             }
 
@@ -103,53 +108,50 @@ namespace circle_display
                 cnvsCircles.Children.Add(line);
 
                 // Tekent de labels bij de checkboxen met het aantal kolommen aangeduid
-                Label newLbl = new Label();
-                newLbl.Content = j.ToString();
-                newLbl.Height = newLbl.Width = 30;
-                newLbl.HorizontalContentAlignment = HorizontalAlignment.Center;
-                newLbl.Margin = new Thickness(j * 30 + 43, circleCenter + 90 + numberOfLeds * 30, 0, 0);
-                cnvsCircles.Children.Add(newLbl);
-            }
+                Label lblKolom = new Label();
 
-            for (int l = 0; l < 32; l++)
-            {
-                Label newLbl = new Label();
+                lblKolom.Content = j.ToString();
+                lblKolom.Height = lblKolom.Width = 30;
+                lblKolom.HorizontalContentAlignment = HorizontalAlignment.Center;
+                lblKolom.Margin = new Thickness(j * 30 + 43, circleCenter + 90 + numberOfLeds * 30, 0, 0);
+                cnvsCircles.Children.Add(lblKolom);
 
-                newLbl.Content = (31 - l).ToString();
-                newLbl.Height = newLbl.Width = 30;
-                newLbl.HorizontalAlignment = HorizontalAlignment.Left;
-                newLbl.VerticalAlignment = VerticalAlignment.Top;
+                // Tekent de labels bij de cirkel kolommen.
+                Label lblCirkel = new Label();
 
-                PointF centerCirkel = new PointF(circleCenter, circleCenter);
-                PointF endCirkel = new PointF((float)Convert.ToDecimal(circleCenter + Math.Cos(l * angle1)), (float)Convert.ToDecimal(circleCenter + Math.Sin(-l * angle1)));
+                lblCirkel.Content = (31 - j).ToString();
+                lblCirkel.Height = lblCirkel.Width = 30;
+                lblCirkel.HorizontalAlignment = HorizontalAlignment.Left;
+                lblCirkel.VerticalAlignment = VerticalAlignment.Top;
 
-                PointF coord1;
-                PointF coord2;
+                endCirkel = new PointF((float)Convert.ToDecimal(circleCenter + Math.Cos(j * angle1)), (float)Convert.ToDecimal(circleCenter + Math.Sin(-j * angle1)));
 
                 Calculations.FindLineCircleIntersections(circleCenter, circleCenter, circleCenter, centerCirkel, endCirkel, out coord1, out coord2);
 
-                newLbl.Margin = new Thickness(coord1.X - 10, coord1.Y - 15, 0, 0);
-                cnvsCircles.Children.Add(newLbl);
+                lblCirkel.Margin = new Thickness(coord1.X - 10, coord1.Y - 15, 0, 0);
+                cnvsCircles.Children.Add(lblCirkel);
             }
 
-
-            // Tekent de bollen die de leds representateren op de snijpunten van de ellipsen met de rechten.
-            for (int l = 0; l < 32; l++)
+            for (int j = 0; j < 32; j++)
             {
-                for (int i = 0; i < 16; i++) 
+                for (int i = 0; i < 16; i++)
                 {
+                    // Tekent de checkboxen die de data array moeten voorstellen
+                    System.Windows.Controls.CheckBox newChb = new CheckBox();
+
+                    newChb.Tag = (j).ToString() + "," + (15 -i).ToString();
+                    newChb.Checked += NewChb_Checked;
+                    newChb.Unchecked += NewChb_Unchecked;
+                    newChb.Margin = new Thickness(j * 30 + 50, i * 30 + circleCenter + 100, 0, 0);
+                    cnvsCircles.Children.Add(newChb);
+
+                    // Tekent de bollen die de leds representateren op de snijpunten van de ellipsen met de rechten.
                     Ellipse cirkelLabel = new Ellipse();
-                    cirkelLabel.Stroke = new SolidColorBrush(Colors.Black);
                     cirkelLabel.Fill = new SolidColorBrush(Colors.Black);
-                    cirkelLabel.Width = cirkelLabel.Height = 20;
                     cirkelLabel.MouseDown += CirkelLabel_MouseDown;
-                    cirkelLabel.Tag = (15 - i).ToString() + "," + (31 - l).ToString();
+                    cirkelLabel.Tag = j.ToString() + "," + (15 -i).ToString();
 
-                    PointF centerCirkel = new PointF(circleCenter, circleCenter);
-                    PointF endCirkel = new PointF((float)Convert.ToDecimal(circleCenter + Math.Cos(l * angle1)), (float)Convert.ToDecimal(circleCenter + Math.Sin(-l * angle1)));
-
-                    PointF coord1;
-                    PointF coord2;
+                    endCirkel = new PointF((float)Convert.ToDecimal(circleCenter - Math.Cos(j * angle1)), (float)Convert.ToDecimal(circleCenter + Math.Sin(-j * angle1)));
 
                     Calculations.FindLineCircleIntersections(circleCenter, circleCenter, (circleCenter - ledDistance) - i * ledDistance, centerCirkel, endCirkel, out coord1, out coord2);
 
@@ -163,24 +165,23 @@ namespace circle_display
         {
             CheckBox cb = sender as CheckBox;
             Ellipse cirkelLabel = new Ellipse();
-            cirkelLabel.Stroke = new SolidColorBrush(Colors.Black);
-            cirkelLabel.StrokeThickness = 1;
             cirkelLabel.Fill = new SolidColorBrush(Colors.Black);
-            cirkelLabel.Width = cirkelLabel.Height = 20;
             string currentEllipse = (string)cb.Tag;
             string[] coords = currentEllipse.Split(',');
             cirkelLabel.Tag = cb.Tag;
             cirkelLabel.MouseDown += CirkelLabel_MouseDown;
 
-            PointF centerCirkel = new PointF(circleCenter, circleCenter);
-            PointF endCirkel = new PointF((float)Convert.ToDecimal(circleCenter + Math.Cos((Convert.ToDouble(coords[1]) - 31) * angle1)), (float)Convert.ToDecimal(circleCenter + Math.Sin(-1 * (Convert.ToDouble(coords[1])) * angle1)));
+            endCirkel = new PointF((float)Convert.ToDecimal(circleCenter - Math.Cos((Convert.ToDouble(coords[0])) * angle1)), (float)Convert.ToDecimal(circleCenter + Math.Sin(-1 * (Convert.ToDouble(coords[0])) * angle1)));
 
-            PointF coord1;
-            PointF coord2;
-
-            Calculations.FindLineCircleIntersections(circleCenter, circleCenter, circleCenter - ledDistance - ((float)Convert.ToDecimal(coords[0])) * ledDistance, centerCirkel, endCirkel, out coord1, out coord2);
+            Calculations.FindLineCircleIntersections(circleCenter, circleCenter, circleCenter - ledDistance - ((float)Convert.ToDecimal(coords[1])) * ledDistance, centerCirkel, endCirkel, out coord1, out coord2);
             cirkelLabel.Margin = new Thickness(coord1.X - 10, coord1.Y - 10, 0, 0);
             cnvsCircles.Children.Add(cirkelLabel);
+
+            DataByte[Convert.ToInt16(coords[0]), Convert.ToInt16(coords[1])].brightness = 0;
+            DataByte[Convert.ToInt16(coords[0]), Convert.ToInt16(coords[1])].red = 0;
+            DataByte[Convert.ToInt16(coords[0]), Convert.ToInt16(coords[1])].green = 0;
+            DataByte[Convert.ToInt16(coords[0]), Convert.ToInt16(coords[1])].blue = 0;
+
         }
 
         private void NewChb_Checked(object sender, RoutedEventArgs e)
@@ -189,24 +190,24 @@ namespace circle_display
             Ellipse cirkelLabel = new Ellipse();
             SolidColorBrush ledFill = new SolidColorBrush(System.Windows.Media.Color.FromRgb(Convert.ToByte(sldRed.Value), Convert.ToByte(sldGreen.Value), Convert.ToByte(sldBlue.Value)));
             cirkelLabel.Fill = ledFill;
-            cirkelLabel.StrokeThickness = 1;
-            cirkelLabel.Stroke = new SolidColorBrush(Colors.Black);
-            cirkelLabel.Width = cirkelLabel.Height = 20;
             string currentEllipse = (string)cb.Tag;
             string[] coords = currentEllipse.Split(',');
             cirkelLabel.Tag = cb.Tag;
             cirkelLabel.MouseDown += CirkelLabel_MouseDown;
 
-            PointF centerCirkel = new PointF(circleCenter, circleCenter);
-            PointF endCirkel = new PointF((float)Convert.ToDecimal(circleCenter + Math.Cos((Convert.ToDouble(coords[1]) - 31) * angle1)), (float)Convert.ToDecimal(circleCenter + Math.Sin(-1 * (Convert.ToDouble(coords[1])) * angle1)));
+            endCirkel = new PointF((float)Convert.ToDecimal(circleCenter - Math.Cos((Convert.ToDouble(coords[0])) * angle1)), (float)Convert.ToDecimal(circleCenter + Math.Sin(-1 * (Convert.ToDouble(coords[0])) * angle1)));
 
-            PointF coord1;
-            PointF coord2;
-
-            Calculations.FindLineCircleIntersections(circleCenter, circleCenter, circleCenter - ledDistance - ((float)Convert.ToDecimal(coords[0])) * ledDistance, centerCirkel, endCirkel, out coord1, out coord2);
+            Calculations.FindLineCircleIntersections(circleCenter, circleCenter, circleCenter - ledDistance - ((float)Convert.ToDecimal(coords[1])) * ledDistance, centerCirkel, endCirkel, out coord1, out coord2);
             cirkelLabel.Margin = new Thickness(coord1.X - 10, coord1.Y - 10, 0, 0);
 
             cnvsCircles.Children.Add(cirkelLabel);
+
+
+            DataByte[Convert.ToInt16(coords[0]), Convert.ToInt16(coords[1])].brightness = sldIntensity.Value;
+            DataByte[Convert.ToInt16(coords[0]), Convert.ToInt16(coords[1])].red = sldRed.Value;
+            DataByte[Convert.ToInt16(coords[0]), Convert.ToInt16(coords[1])].green = sldGreen.Value;
+            DataByte[Convert.ToInt16(coords[0]), Convert.ToInt16(coords[1])].blue = sldBlue.Value;
+            
         }
 
         private void CirkelLabel_MouseDown(object sender, MouseButtonEventArgs e)
@@ -230,6 +231,20 @@ namespace circle_display
             sldRed.Value = 255;
             sldGreen.Value = 255;
             sldBlue.Value = 255;
+        }
+
+        private void btnExport_Click(object sender, RoutedEventArgs e)
+        {
+            using (StreamWriter writeData = new StreamWriter("SendData.txt"))
+            
+            for (int i = 0; i < 32; i++)
+            {
+                for (int j = 0; j < 16; j++)
+                {
+                    writeData.Write($"{Convert.ToString(DataByte[i, j].brightness)},{Convert.ToString(DataByte[i, j].red)},{Convert.ToString(DataByte[i, j].green)},{Convert.ToString(DataByte[i, j].blue)},");
+                }
+            }
+            
         }
     }
 }
